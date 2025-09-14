c92e599888133114564cebf1e60058a46db01089
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2025-09-14 14:39:22 +0800

    Time: 13 ms (36.82%), Space: 31.9 MB (82.28%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -9,8 +9,10 @@ public:
     {
         sort(nums.begin(), nums.end());
         int sum = 0;
-        for (int i = 0; i < nums.size() - 1; i += 2)
-            sum += min(nums[i], nums[i + 1]);
+        // for (int i = 0; i < nums.size() - 1; i += 2)
+        //     sum += min(nums[i], nums[i + 1]);    其實排序後只需要加上(a,b)的a(較小)就好,min多此一舉
+        for (int i = 0; i < nums.size(); i += 2)
+            sum += nums[i];
         return sum;
     }
 };
