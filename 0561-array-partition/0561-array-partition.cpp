d8f782fb6214c3f8f1a5c0ddab74f16f96d2bc9c
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2025-09-14 14:38:00 +0800

    Time: 22 ms (5.23%), Space: 32.1 MB (54.7%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,16 @@
+// key:小的盡量跟小的在同一個()裡,大的number才會有所使用,不然都被min出去
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    int arrayPairSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size() - 1; i += 2)
+            sum += min(nums[i], nums[i + 1]);
+        return sum;
+    }
+};
