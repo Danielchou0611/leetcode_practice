1bc7336f5ba05a46fa28a74f8f730668486844b5
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2026-01-01 16:52:16 +0800

    Time: 7 ms (86.05%), Space: 9.5 MB (56.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,44 +1,34 @@
-/*
-    Solution 2 : 利用中心點往左右找
-    Time Complexity: O(n^2)
-    Space Complexity: O(n^2)
- */
+// Solution 3: 將Solution 2改為function的模式
 class Solution
 {
 public:
+    int start;
+    int max_len;
     string longestPalindrome(string s)
     {
         int length = s.length();
-        int start = 0, max_len = 1;
+        start = 0, max_len = 1;
         // 宣告一個lambda function，[&]的意思是capture by reference，意思是可以直接修改外部變數max_len跟start
-        auto expand = [&](int left, int right)
-        {
-            while (left >= 0 && right < length && s[left] == s[right])
-            {
-                if (right - left + 1 > max_len)
-                {
-                    max_len = right - left + 1;
-                    start = left;
-                }
-                left--;
-                right++;
-            }
-        };
 
         for (int i = 0; i < length; i++)
         {
-            expand(i, i);     // odd length，exL abbd最後只有max_len=1
-            expand(i, i + 1); // even length，ex: abbd的中心在bb
+            expand(s, i, i);     // odd length，exL abbd最後只有max_len=1
+            expand(s, i, i + 1); // even length，ex: abbd的中心在bb
         }
         return s.substr(start, max_len);
     }
+    void expand(const string &s, int left, int right) // 用reference就不用複製字串，效率更好,const: read only
+    {
+        int length = s.length();
+        while (left >= 0 && right < length && s[left] == s[right])
+        {
+            if (right - left + 1 > max_len)
+            {
+                max_len = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
 };
-/*
-    [捕捉方式](參數) -> 回傳型別
-    []：捕捉清單 (capture list)，決定 Lambda 可以存取哪些外部變數。
-    常見用法：
-    [&] → 捕捉所有外部變數 以引用。
-    [=] → 捕捉所有外部變數 以值複製。
-    [&x, y] → 捕捉 x 以引用，y 以值。
-    [this] → 捕捉當前物件的 this 指標。
-*/
