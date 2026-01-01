9a07fc9e0cc30e8b3dbf1c7e86554a3017bfa9dd
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2026-01-01 16:43:35 +0800

    Time: 15 ms (54.15%), Space: 9.4 MB (62.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,44 @@
-#include <string>
-#include <vector>
-using namespace std;
+/*
+    Solution 2 : 利用中心點往左右找
+    Time Complexity: O(n^2)
+    Space Complexity: O(n^2)
+ */
 class Solution
 {
 public:
-    string longestPalindrome(string s) // 找出最長回文
+    string longestPalindrome(string s)
     {
-        string result = "";
         int length = s.length();
-        if (length == 1)
-            return s;
-        vector<vector<bool>> dp(length, vector<bool>(length, false));
-        int start = 0, end = 0, max_len = 1;
+        int start = 0, max_len = 1;
+        // 宣告一個lambda function，[&]的意思是capture by reference，意思是可以直接修改外部變數max_len跟start
+        auto expand = [&](int left, int right)
+        {
+            while (left >= 0 && right < length && s[left] == s[right])
+            {
+                if (right - left + 1 > max_len)
+                {
+                    max_len = right - left + 1;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+        };
+
         for (int i = 0; i < length; i++)
         {
-            dp[i][i] = true;
-            for (int j = 0; j < i; j++)
-            {
-                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1]))   //長度如果小於3就一定是回文
-                {
-                    dp[j][i] = true;
-                    if (i - j + 1 > max_len)
-                    {
-                        max_len = i - j + 1;
-                        start = j;
-                        end = i;
-                    }
-                }
-            }
+            expand(i, i);     // odd length，exL abbd最後只有max_len=1
+            expand(i, i + 1); // even length，ex: abbd的中心在bb
         }
-        return s.substr(start, end - start + 1);
+        return s.substr(start, max_len);
     }
 };
+/*
+    [捕捉方式](參數) -> 回傳型別
+    []：捕捉清單 (capture list)，決定 Lambda 可以存取哪些外部變數。
+    常見用法：
+    [&] → 捕捉所有外部變數 以引用。
+    [=] → 捕捉所有外部變數 以值複製。
+    [&x, y] → 捕捉 x 以引用，y 以值。
+    [this] → 捕捉當前物件的 this 指標。
+*/
