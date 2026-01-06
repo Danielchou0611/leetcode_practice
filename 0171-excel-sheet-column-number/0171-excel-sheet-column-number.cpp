bfe5849bc38ef1900c80eaeebb72c5410c871caf
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2026-01-06 21:45:11 +0800

    Time: 0 ms (100%), Space: 8.7 MB (72.31%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*
+    解法ex: ZAY = (ZA)*26+Y = ((Z*26)+A)*26+Y
+    time complexity: O(n)
+    space complexity: O(1)
+*/
 class Solution
 {
 public:
     int titleToNumber(string columnTitle)
     {
-        int i = 0, count = 0;
-        while (i < columnTitle.length())
-        {
-            count = count * 26 + (columnTitle[i] - 'A' + 1);
-            i++;
-        }
+        long long count = 0;       // long long防止溢位
+        for (char c : columnTitle) // 遍歷字串
+            count = count * 26 + (c - 'A' + 1);
         return count;
     }
 };
