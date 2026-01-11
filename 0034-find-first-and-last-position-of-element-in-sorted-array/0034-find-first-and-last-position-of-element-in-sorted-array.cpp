fc2d079b0c7d9800c0d3f7772d8b0211e2e16d6b
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2026-01-11 21:37:35 +0800

    Time: 0 ms (100%), Space: 17.5 MB (85.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,37 @@
+//    Solution 2: Solution 1 的精簡版
 class Solution
 {
 private:
-    int start, end;
+    int findbound(vector<int> &nums, int target, int isFirst)
+    {
+        int left = 0, right = nums.size() - 1;
+        int bound = -1;
+        while (left <= right)
+        {
+            int mid = left + ((right - left) >> 1); //+優先於>>
+            if (nums[mid] == target)
+            {
+                bound = mid; // 當前位置
+                if (isFirst) // 在mid左半部找更左邊的邊界(左邊可能還有)
+                    right = mid - 1;
+                else // 在mid右半部找更右邊的邊界(右邊可能還有)
+                    left = mid + 1;
+            }
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return bound;
+    }
 
 public:
     vector<int> searchRange(vector<int> &nums, int target)
     {
-        this->start = -1;
-        this->end = -1;
-        binary_search(nums, 0, nums.size() - 1, target);
-        vector<int> result;
-        result.push_back(start);
-        result.push_back(end);
-        return result;
-    }
-    void binary_search(vector<int> &nums, int left, int right, int target)
-    {
-        if (left > right)
-            return;
-        int mid = (left + right) / 2;
-        if (nums[mid] == target)
-        {
-            if (this->start == -1)
-            {
-                this->start = mid;
-                this->end = mid;
-            }
-            else if (mid > end)
-                end = mid;
-            else if (mid < start)
-                start = mid;
-            binary_search(nums, left, mid - 1, target);
-            binary_search(nums, mid + 1, right, target);
-        }
-        else if (nums[mid] > target)
-            binary_search(nums, left, mid - 1, target);
-        else
-            binary_search(nums, mid + 1, right, target);
+        int first = findbound(nums, target, true);
+        if (first == -1)
+            return {-1, -1};
+        int last = findbound(nums, target, false);
+        return {first, last};
     }
 };
