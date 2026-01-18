04862211876189878d2dffb900a6e8b7b6bf614c
Danielchou <105227549+Danielchou0611@users.noreply.github.com>
2026-01-18 17:22:27 +0800

    Time: 0 ms (100%), Space: 17.3 MB (39.69%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,7 +1,5 @@
-#include <vector>
-using namespace std;
 /*
-    Solution: 重複買賣的max_profit為每個遞增區間的差間加總
+    Solution 2: 只要現在的比前一個高價就加進去max_profit
     time complexity: O(n)
     space complexity: O(1)
 */
@@ -10,16 +8,14 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
-        int profit = 0, min_price = prices[0], max_price = prices[0]; // price>=0
+        int profit = 0; // price>=0
         for (int i = 1; i < prices.size(); i++)
         {
-            if (prices[i] < prices[i - 1])
+            if (prices[i] > prices[i - 1])
             {
-                profit += max_price - min_price;
-                min_price = prices[i];
+                profit += prices[i] - prices[i - 1];
             }
-            max_price = prices[i];
         }
-        return profit + max_price - min_price;
+        return profit;
     }
 };
